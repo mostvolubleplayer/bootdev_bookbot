70e584cec6e8e0bf121b5ebf3577ff26c6c3190e
mostvolubleplayer <jt.tocb@gmail.com>
2025-03-25 14:50:38 -0400

    updated main.py with the ability to count words

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 
+
 def main():
     output = get_book_text(filepath)
-    print(output)
+    word_list = output.split() 
+    num_words = len(word_list)
+    print(f"{num_words} words found in the document")
+
+    
 
 main()
