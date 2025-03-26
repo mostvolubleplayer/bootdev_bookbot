ca2c59c2a09cc9e115086a2a869938d62321e554
mostvolubleplayer <jt.tocb@gmail.com>
2025-03-26 09:51:21 -0400

    L2.6 - Count Characters - added code to provide individual letter counts

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count
 
 def main():
     print(f"{get_word_count()} words found in the document")
+
+    char_counts = get_character_count()
+    print("Character counts:")
+    for char, count in char_counts.items():
+        print(f"'{char}': {count}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -12,6 +12,20 @@ def get_word_count():
     num_words = len(word_list)
     return num_words
 
-
-
-
+# to make a function call get_character_count, I need to:
+# 1. in STATS, break the book text into a dictionary of characters
+# 2. Convert any upper case characters into lower case characters
+# 3. set up the dictionary so that each instance of a character += an individual character counter in the dictionary
+# 4. return those results to the main.py
+# 5. Print those results using a get_character_count function call.
+def get_character_count():
+    char_count = {}
+    text = get_book_text(filepath).lower()
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+        
+        
